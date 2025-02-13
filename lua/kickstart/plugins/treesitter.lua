7910a1fbb6aa170f16f15712bb21f17dc207c62c
harikris <harikrishnanp4274@gmail.com>
2025-02-13 17:18:26 +0530

    updated config

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -25,4 +25,4 @@ return {
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 }
--- vim: ts=2 sts=2 sw=2 et
+-- vim: ts=8 sts=2 sw=2 noet
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -20,13 +20,33 @@ require('lazy').setup({
   -- Use `opts = {}` to force a plugin to be loaded.
   --
 
-
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
   require 'kickstart/plugins/gitsigns',
 
+  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+  --
+  -- This is often very useful to both group configuration, as well as handle
+  -- lazy loading plugins that don't need to be loaded immediately at startup.
+  --
+  -- For example, in the following configuration, we use:
+  --  event = 'VimEnter'
+  --
+  -- which loads which-key before all the UI elements are loaded. Events can be
+  -- normal autocommands events (`:help autocmd-events`).
+  --
+  -- Then, because we use the `opts` key (recommended), the configuration runs
+  -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
+
   require 'kickstart/plugins/which-key',
+
+  -- NOTE: Plugins can specify dependencies.
+  --
+  -- The dependencies are proper plugin specifications as well - anything
+  -- you do for a plugin at the top level, you can do for a dependency.
+  --
+  -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
   require 'kickstart/plugins/telescope',
 
@@ -91,4 +111,4 @@ require('lazy').setup({
   },
 })
 
--- vim: ts=2 sts=2 sw=2 et
+-- vim: ts=8 sts=2 sw=2 noet
