4994b5dac0154d77f204b0953925fab0a1c25baa
harikris <harikrishnanp4274@gmail.com>
2025-03-04 17:30:53 +0530

    updated config + added nvim-window-picker, image.nvim, vim-kitty

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -20,9 +20,13 @@ return {
   opts = {
     filesystem = {
       window = {
+        position = 'right',
         mappings = {
           ['\\'] = 'close_window',
         },
+      },
+      filtered_items = {
+        visible = true,
       },
     },
   },
