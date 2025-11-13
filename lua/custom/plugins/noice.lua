f77883170eeb46c8e72c2deb1f6c02fb9e72ffaa
harikrishnanp <harikrishnanp4274@gmail.com>
2025-11-13 14:57:38 +0530

    disabled automatic display of signature help in insert mode

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -12,6 +12,18 @@ return {
         ['vim.lsp.util.stylize_markdown'] = true,
         ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
       },
+      signature = {
+        enabled = true,
+        auto_open = {
+          enabled = false,
+          trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+          luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+          throttle = 50, -- Debounce lsp signature help request by 50ms
+        },
+        view = nil, -- when nil, use defaults from documentation
+        ---@type NoiceViewOptions
+        opts = {}, -- merged with defaults from documentation
+      },
     },
     -- you can enable a preset for easier configuration
     presets = {
@@ -19,14 +31,7 @@ return {
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false, -- add a border to hover docs and signature help
-    },
-    -- Show @recording messages
-    routes = {
-      {
-        view = 'notify',
-        filter = { event = 'msg_showmode' },
-      },
+      lsp_doc_border = true, -- add a border to hover docs and signature help
     },
   },
   dependencies = {
