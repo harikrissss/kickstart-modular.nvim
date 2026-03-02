b98bb04d58ee4dca7d784583b41a2512829eb707
harikrishnanp <harikrishnanp4274@gmail.com>
2026-03-02 11:38:28 +0530

    sshfs within neovim

---
+++ lua/custom/plugins/sshfs.lua
@@ -0,0 +1,22 @@
+return {
+  'uhs-robert/sshfs.nvim',
+  opts = {
+    host_paths = {
+      -- Optionally define default mount paths for specific hosts
+      -- These are shown in addition to global_paths
+      -- Single path (string):
+      -- ["my-server"] = "/var/www/html"
+      --
+      -- Multiple paths (array):
+      -- ["dev-server"] = { "/var/www", "~/projects", "/opt/app" }
+      ['10.97.32.101'] = '~/hari',
+      ['10.97.32.122'] = '~/hari',
+      ['10.97.32.105'] = '~/hari',
+    },
+    hooks = {
+      on_exit = {
+        auto_unmount = false, -- auto-disconnect all mounts on :q or exit
+      },
+    },
+  },
+}
